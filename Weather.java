00b87e3530a6d5e9544835364ce77769c6304b5f
kannan1337 <84678086+kannan1337@users.noreply.github.com>
2021-07-29 20:44:10 -0400

    Added comments. Made code more custom.
    Simplified.
    To submit with project update.

--- Weather.java
+++ Weather.java
@@ -1,8 +1,14 @@
+/**
+   Uses city as input to create a weather object that stores various
+   weather properties of that city.
+   Provides methods to cahnge city and retrieve weather properties.
+*/
+
 import com.google.gson.*;
 import com.google.gson.reflect.*;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.util.Scanner;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
@@ -10,10 +16,21 @@ import java.util.Map;
 
 public class Weather
 {
+   // This is the API key from OpenWeatherMap.org specific to a new user account.
+   // The free account holds some limitations.
    private static final String WEATHER_API_KEY = "66942c22b4b7ec481ba04d34e010b30e";
-   private String city, weatherUrlString;
+   
+   // Holds city name, and the URL that gives the weather report as json string
+   private String city, weatherUrlString, report;  // String to hold report
+   
+   // Hold temperature value as an int
    private int temperature;
    
+   /**
+      Converts json string to a map.
+      This piece of code was adapted from the internet.
+      Source will be added in next update.
+   */
    public static Map<String, Object> jsonToMap(String str)
    {
       Map<String, Object> map = new Gson().fromJson(
@@ -22,21 +39,33 @@ public class Weather
          );
          return map;
    }
-
+   
+   /*
+      Constructor that calls updateCity method with the param
+      and then updates the weather using update method.
+      @param pCity Name of the city
+   */
    public Weather(String pCity)
    {
       updateCity(pCity);
       update();
    }
    
+   /**
+      Updates the City
+      @param pCity Name of the city
+   */
    public void updateCity(String pCity)
    {
+      // Update the city
       city = pCity;
       
+      // Create new URL to query the weather with updated city
       weatherUrlString = "http://api.openweathermap.org/data/2.5/weather?q="
          + city + "&appid=" + WEATHER_API_KEY + "&units=imperial";
       System.out.println("Weather query URL: " + weatherUrlString + "\n");
       
+      // Call update method to get new weather report
       update();
    }
    
@@ -44,35 +73,52 @@ public class Weather
    {
       try
       {
-         StringBuilder report = new StringBuilder();
+         // URL object created from the URL string
          URL weatherUrl = new URL(weatherUrlString);
+      
+         // A connection is created using the URL
          URLConnection urlConn = weatherUrl.openConnection();
+      
+         // The text received from URL is stored
          InputStreamReader inputStreamReader = new InputStreamReader(urlConn.getInputStream());
-         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-         String inputLine;
-         while ((inputLine = bufferedReader.readLine()) != null)
-         {
-            // System.out.println(inputLine);
-            report.append(inputLine);
-         }
-         bufferedReader.close();
+      
+         // Create scanner object to hold the received text
+         Scanner scanner = new Scanner(inputStreamReader);
+      
+         // Save the text to report string
+         report = scanner.nextLine();
+      
+         // Print out the report for testing
          System.out.println("Weather report is:\n" + report + "\n");
          
+         // Convert report string to a Map using google's gson code
          Map<String, Object> reportMap = jsonToMap(report.toString());
+      
+         // Convert the "main" field of the reportMap to a new map for main
          Map<String, Object> mainMap = jsonToMap(reportMap.get("main").toString());
          
-         temperature = (int) Float.parseFloat(mainMap.get("temp").toString());         
+         // Retrieve the temperature from mainmap, convert to float, then to int
+         // and save in temperature field
+         temperature = (int) Float.parseFloat(mainMap.get("temp").toString());
       } catch (IOException exception)
       {
          System.out.println(exception.getMessage());
-      }
+      }      
    }
    
+   /**
+      Getter method for temperature
+      @return Current temperature for the city
+   */
    public int getTemp()
    {
       return temperature;
    }
    
+   /**
+      Getter method for city
+      @return Current city
+   */
    public String getCity()
    {
       return city;
