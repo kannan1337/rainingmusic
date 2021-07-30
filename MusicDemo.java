20d4bc615ea30c85640d0895ce14ddfd88a21ccb
kannan1337 <84678086+kannan1337@users.noreply.github.com>
2021-07-29 20:53:17 -0400

    Added comments

--- MusicDemo.java
+++ MusicDemo.java
@@ -1,41 +1,70 @@
+/**
+   This program demos the use of weather class and uses its temperature
+   return method to play four chords with that tempo
+*/
+
 import java.util.Scanner;
 import org.jfugue.player.Player;
 
 public class MusicDemo
 {
+   // To hold any input from customer
    private static String input;
+   
+   // To hold weather report
    private static Weather weather;
    
+   /**
+      Main method for the demo
+   */
    public static void main(String[] args)
    {
+      // Create a new jfuge music player
       Player player = new Player();
       
-    
+      // To hold use input as scanner object
       Scanner scanner = new Scanner(System.in);
+      
+      // Ask user for city to pull weather for
       System.out.print("Enter city: ");
+      
+      // City name is stores in input string
       input = scanner.nextLine();
       
+      // Create a Weather object using constructor that passes city name into it
       weather = new Weather(input);
       
+      // Print the temperature using the getter method from the weather object
       System.out.println(weather.getCity() + " temperature: " + weather.getTemp());
       
+      // Play 4 chords using the temperature as the tempo
       player.play("T" + String.valueOf(weather.getTemp()) + " C D E F");
       
+      // Ask user whether to change the city
       System.out.print("\nChange city? (Y/N): ");
+      
+      // Store user input
       input = scanner.nextLine();
       
-      
+      // Proceed with loop only if user said they want to update the city
       while (input.charAt(0) == 'Y' || input.charAt(0) == 'y')
       {
+         // Ask user for new city and store input in string
          System.out.print("\nEnter new city: ");
          input = scanner.nextLine();
-         weather.updateCity(input);
-         System.out.println(weather.getCity() + " temperature: " + weather.getTemp());
-         player.play("T" + String.valueOf(weather.getTemp()) + " C D E F");
-         System.out.print("\nChange city? (Y/N): ");
-         input = scanner.nextLine();
          
-      }
-      
+         // Call the weather object's updateCity method to change the city
+         weather.updateCity(input);
+         
+         // Print temperature
+         System.out.println(weather.getCity() + " temperature: " + weather.getTemp());
+         
+         // Play 4 chords using the temperature as the tempo
+         player.play("T" + String.valueOf(weather.getTemp()) + " C D E F");
+         
+         // Ask user whether to change the city and store input
+         System.out.print("\nChange city? (Y/N): ");
+         input = scanner.nextLine();  
+      }      
    }
 }
