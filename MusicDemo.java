174aa3ef7941e463d10f1587c7aaaa6e7759968c
kannan1337 <kannanplus@gmail.com>
2021-08-11 23:28:54 -0400

    Removed commented lines & cleaned up

--- MusicDemo.java
+++ MusicDemo.java
@@ -3,33 +3,28 @@
    return method to play four chords with that tempo
 */
 
+// To get user input
 import java.util.Scanner;
-import org.jfugue.player.Player;
 
 public class MusicDemo
 {
-   // To hold any input from customer
-   private static String input;
-   
-   // To hold weather report
-   private static Weather weather;
-   
-   // To hold object that contains countries-continents list
-   //private static CountryContinentMap countries;
-   
    /**
       Main method for the demo
    */
+   
    public static void main(String[] args)
    {
-      // Create a new jfuge music player
-      Player player = new Player();
+      // To hold any input from customer
+      String input;
+      
+      // To hold weather report
+      Weather weather;
+      
+      // To hold a Music class or subclass that plays the music
+      Music music;
       
       // To hold use input as scanner object
       Scanner scanner = new Scanner(System.in);
-      
-      // Call constructor to create map with countries & continents
-      //countries = new CountryContinentMap();
       
       // Ask user for city to pull weather for
       System.out.print("Enter city: ");
@@ -49,38 +44,8 @@ public class MusicDemo
          // + ", type: " + weather.getWeatherType();
       
       // Play cold music depending on continent
-      Music coldMusic = new ColdMusic();
-      coldMusic.play(weather);
-      
-      // Asking user for another city is current disabled till it is completed.
-      /*
-      // Ask user whether to change the city
-      System.out.print("\nChange city? (Y/N): ");
-      
-      // Store user input
-      input = scanner.nextLine();
-      
-      // Proceed with loop only if user said they want to update the city
-      while (input.charAt(0) == 'Y' || input.charAt(0) == 'y')
-      {
-         // Ask user for new city and store input in string
-         System.out.print("\nEnter new city: ");
-         input = scanner.nextLine();
-         
-         // Call the weather object's updateCity method to change the city
-         weather.updateCity(input);
-         
-         // Print temperature
-         System.out.println(weather.getCity() + " temperature: " + weather.getTemp());
-         
-         // Play 4 chords using the temperature as the tempo
-         player.play("T" + String.valueOf(weather.getTemp()) + " C D E F");
-         
-         // Ask user whether to change the city and store input
-         System.out.print("\nChange city? (Y/N): ");
-         input = scanner.nextLine();
-      
-      }
-      */      
+      music = new ColdMusic();
+      music.play(weather);
+            
    }
 }
