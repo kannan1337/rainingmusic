244dd85c719abeaec325c574572846980ca8972b
kannan1337 <kannanplus@gmail.com>
2021-08-15 21:16:47 -0400

    Modified LocationHelp for autocomplete feature
    This is really part of previous commit but could not be committed together since this file was renamed in last commit

--- LocationHelp.java
+++ LocationHelp.java
@@ -5,22 +5,48 @@
    and country name when 2 letter country code is available.
 */
 
+// To read file contents
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public final class CountryContinentMap
-{      
+// To read the resulting weather report file/string from visiting URL
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// Used to retrieve json string weather report by connecting to a URL
+import java.net.URL;
+import java.net.URLConnection;
+
+// Json.Simple is used to extract an array from json string
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+public final class LocationHelp
+{
+   /*
+      This is the API key for HERE Location Services REST APIs specific to the
+      programmer's user account. It expires every few days or so and requires
+      renewal.
+   */
+   private static final String LOCATION_API_KEY = "hTJHRgc45thH3h69WBNC5Y7FRRTfEWSDBt65x9bCegE";
+   
+   // To hold location suggestions json string retrieved from web service
+   private static String report;
+   
    // Initialize fields with default values of USA
    private static String continentCode = "NA",
       continent = "North America",
+      countryCode = "US",
       country = "United States of America";
    
    /**
       Sets continent code  and continent for given country
-      @param countryCode 2 letter country code
+      @param countryCode2Letter Country code (2 or 3 letters)
    */
-   private static void getData(String countryCode)
+   private static void getData(String pCountryCode)
    {
       try
       {
@@ -36,23 +62,37 @@ public final class CountryContinentMap
          // Continues loop till end of file
          while (scanner.hasNext()) 
          {
-            // Checks if first string of line is the country code
-            if(scanner.next().equals(countryCode))
+            // Check first two colums before skipping to next line
+            for (byte i = 0; i < 2; i++)
             {
-               // Copy next column to continent code
-               continentCode = scanner.next();
-               
-               // Copy next column to continent
-               continent = scanner.next();
-               
-               // Skip the three letter country code column
-               scanner.next();
-               
-               // Copy the next column to country
-               country = scanner.next();
-               
-               // Break if country found
-               break;
+               // Checks if first string of line is the country code
+               if(scanner.next().equals(pCountryCode))
+               {
+                  // If 3 letter country code is passed
+                  if(pCountryCode.length() == 3)
+                  {
+                     // Copy next column to country code to change to 2 letter code
+                     countryCode = scanner.next();
+                  }
+                  // If two letter country code was passed
+                  else
+                  {
+                     // Copy passed in argument to country code
+                     countryCode = pCountryCode;
+                  }
+                  
+                  // Copy next column to continent code
+                  continentCode = scanner.next();
+                  
+                  // Copy next column to continent
+                  continent = scanner.next();
+                  
+                  // Copy the next column to country
+                  country = scanner.next();
+                  
+                  // Break if country found
+                  break;
+               }
             }
             
             // Skip to next line
@@ -71,13 +111,13 @@ public final class CountryContinentMap
    
    /**
       Returns 2 letter continent code for given 2 letter country code
-      @param countryCode 2 letter country code
+      @param pCountryCode 2 letter country code
       @return 2 letter continent code
    */
-   public static String getContinentCode(String countryCode)
+   public static String getContinentCode(String pCountryCode)
    {
       // Call getData method to change fields with values from csv file
-      getData(countryCode);
+      getData(pCountryCode);
       
       // Return 2 letter continent code
       return continentCode;
@@ -85,13 +125,13 @@ public final class CountryContinentMap
    
    /**
       Returns Continent name for given 2 letter country code
-      @param countryCode 2 letter country code
+      @param pCountryCode 2 letter country code
       @return Continent name
    */
-   public static String getContinent(String countryCode)
+   public static String getContinent(String pCountryCode)
    {
       // Call getData method to change fields with values from csv file
-      getData(countryCode);
+      getData(pCountryCode);
       
       // Return continent name
       return continent;
@@ -99,15 +139,127 @@ public final class CountryContinentMap
    
    /**
       Returns Country name for given 2 letter country code
-      @param countryCode 2 letter country code
+      @param pCountryCode 2 letter country code
       @return Continent name
    */
-   public static String getCountry(String countryCode)
+   public static String getCountry(String pCountryCode)
    {
       // Call getData method to change fields with values from csv file
-      getData(countryCode);
+      getData(pCountryCode);
+      
+      // Return 2 letter country code
+      return countryCode;
+   }
+   
+   /**
+      Returns 2 letter Country Code for given 3 letter country code
+      @param pCountryCode 3 letter country code
+      @return Continent name
+   */
+   public static String convert3to2LetterCountryCode(String pCountryCode)
+   {
+      // Call getData method to change fields with values from csv file
+      getData(pCountryCode);
       
       // Return country name
-      return country;
+      return countryCode;
+   }
+   
+   /**
+      Returns list of location suggestions based on
+      the first few letters of the city passed into
+      this method.
+      @param input First fetters of city entered by user
+      @return Array of 5 top matches of cities
+   */
+   
+   public static Location[] autocomplete(String input)
+   {
+      // Create new URL string to query matched locations with user input
+      String locationUrlString = "https://autocomplete.search.hereapi.com/v1/autocomplete?types=city&limit=5&apiKey="
+         + LOCATION_API_KEY + "&q=" + input;
+            
+      try
+      {
+         
+         // URL object created from the URL string
+         URL locationUrl = new URL(locationUrlString);
+         
+         // A connection is created using the URL
+         URLConnection urlConn = locationUrl.openConnection();
+         
+         // The text received from URL is stored
+         InputStreamReader inputStreamReader = new InputStreamReader(urlConn.getInputStream());
+      
+         // Create scanner object to hold the received text
+         Scanner scanner = new Scanner(inputStreamReader);
+      
+         // Save the text to report string
+         report = scanner.nextLine();
+      } catch (IOException exception)
+      {
+         System.out.println(exception.getMessage());
+      }
+      
+      // Declare array to store 5 locations in Location objects array
+      Location locationSuggestions[] = new Location[5]; 
+      
+      try
+      {
+         // Create Parser for JSON text
+         JSONParser parser = new JSONParser();
+         
+         // Stores parsed JSON text as JSONObject
+         JSONObject locationObj = (JSONObject)parser.parse(report);
+         
+         // Stores the info from weather section of this json string as an array
+         // This is because weather section contains an array with 1 element
+         JSONArray locationObjArray = (JSONArray)locationObj.get("items");
+         
+         for(int i = 0; i < 5; i++)
+         {
+            // Store the first element of locationObjArray as Json object
+            locationObj = (JSONObject)locationObjArray.get(i);
+            
+            // Store the address field in the above as a json object
+            locationObj = (JSONObject)locationObj.get("address");
+            
+            // Store relevant fields from the location retrieved
+            String label = locationObj.get("label").toString();
+            String countryCode = locationObj.get("countryCode").toString();
+            String city = locationObj.get("city").toString();
+                        
+            /*
+               Store USA's state code if country is USA
+               Then call the relevant constructor based on country USA or not
+               to store the i-th location suggestion in to the array of Location objects.
+            */
+            
+            if(countryCode.equals("USA"))
+            {
+               // Store state code field from the location retrieved
+               String usaStateCode = locationObj.get("stateCode").toString();
+               
+               // Call the constructor specific to USA location
+               // Constructor overloading
+               locationSuggestions[i] = new Location(label, city, usaStateCode, countryCode);
+            }
+            
+            // Call constructor for countries outside USA
+            // Constructor overloading
+            else
+            {
+               locationSuggestions[i] = new Location(label, city, countryCode);
+            }
+         }         
+      } catch(ParseException pe)
+      {
+         // Print exception from trying to parse json string
+         System.out.println("position: " + pe.getPosition());
+         System.out.println(pe);
+      }
+      
+      // Return the location suggestions String array
+      return locationSuggestions;
    }
 }
